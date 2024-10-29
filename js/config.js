92768db8324a4ee8987e4a446d102551ada34f0b
Simran <simranroy.186@gmail.com>
2024-10-29 23:18:13 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_qqdy4rPx3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '43m6mdgfs1ag89u4s5ti0qdgr9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
